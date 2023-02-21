74accc80eced17e460abbf54d4423d13b5d0518d
areum <alliejj99@gmail.com>
2023-02-21 18:35:50 +0900

    react에서addEventListener와 같은 기능 사용해보기

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,12 @@
 import { Component } from "react";
 import "./Square.css";
+
 export default class Square extends Component {
   render() {
-    return <button className="square">Square</button>;
+    return (
+      <button className="square" onClick={() => console.log("click")}>
+        {this.props.value}
+      </button>
+    );
   }
 }
