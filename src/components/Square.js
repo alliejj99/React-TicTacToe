9b9c87118f02ee8d9d16ccd5f556291910d27eb0
areum <alliejj99@gmail.com>
2023-02-22 19:29:01 +0900

    react Hooks and class component exchanged functional component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,12 @@
-import { Component } from "react";
+import React from "react";
+
 import "./Square.css";
 
-export default class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
+const Square = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+export default Square;
